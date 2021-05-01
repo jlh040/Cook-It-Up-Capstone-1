d3dfcaee94d63b68c6ca3e86e913197d5b51c548
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-01 13:25:31 -0700

    test that we can see a user in the database

--- test_user_model.py
+++ test_user_model.py
@@ -2,13 +2,38 @@
 
 import os
 from unittest import TestCase
-from models import db, User, Recipe
+from models import db, User, Recipe, UserRecipe
 from secret_keys import API_KEY
 
-os.environ['DATABASE_URL'] = 'cook-it-up-test-db'
+os.environ['DATABASE_URL'] = 'postgresql:///cook-it-up-test-db'
 
 from app import app
 
 app.config['TESTING'] = True
 app.config['WTF_CSRF_ENABLED'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+
+db.create_all()
+
+class UserModelTestCase(TestCase):
+    """Test the functionality of the user model."""
+
+    def setUp(self):
+        """Delete any leftover data."""
+        User.query.delete()
+        Recipe.query.delete()
+        UserRecipe.query.delete()
+
+    def test_create_user(self):
+        """Can we create a new user in the database?"""
+        new_user = User(
+            username='bob8745',
+            password='some_pass12321x',
+            first_name='Bob',
+            email="chico23@gmail.com"
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        
+        self.assertTrue(User.query.count(), 1)
 
