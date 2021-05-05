cddb7f7eea3006bcefc443549bb5f0ab8ff23bcd
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-04 18:02:28 -0700

    changed the seed file again

--- seed.py
+++ seed.py
@@ -1,13 +1,84 @@
 from app import app
 from models import db, User, Recipe, UserRecipe
 
-def fill_recipe_table():
-    for num in range(200001, 500000):
-        recipe = Recipe(api_id=num)
-        db.session.add(recipe)
 
+db.drop_all()
 db.create_all()
 
-fill_recipe_table()
+for num in range(1, 100000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(100000, 200000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(200000, 300000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(300000, 400000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(400000, 500000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(500000, 600000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(600000, 700000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(700000, 800000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(800000, 900000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+    
+for num in range(900000, 1000000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(1000000, 1100000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(1100000, 1200000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
+
+db.session.commit()
+
+for num in range(1200000, 1300000):
+    recipe = Recipe(api_id=num)
+    db.session.add(recipe)
 
 db.session.commit()
