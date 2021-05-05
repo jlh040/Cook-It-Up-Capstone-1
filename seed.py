6ebdd69584307c19ddcc95bede95303431f4350a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-04 17:12:07 -0700

    changed the seed file again

--- seed.py
+++ seed.py
@@ -2,17 +2,12 @@ from app import app
 from models import db, User, Recipe, UserRecipe
 
 def fill_recipe_table():
-    for num in range(1, 600000):
+    for num in range(1, 1300000):
         recipe = Recipe(api_id=num)
         db.session.add(recipe)
-    db.session.commit()
 
 db.drop_all()
 db.create_all()
-
-
-db.session.add_all([test_user_1, test_user_2, test_user_3])
-db.session.commit()
 
 fill_recipe_table()
 
